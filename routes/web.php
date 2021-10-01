0194b62305c7a441713dd1be93f345f03f5a4210
PhanHieu <huyhieup@gmail.com>
2021-10-01 21:52:53 +0700

    as

--- routes/web.php
+++ routes/web.php
@@ -2,26 +2,13 @@
 
 use Illuminate\Support\Facades\Route;
 
-/*
-|--------------------------------------------------------------------------
-| Web Routes
-|--------------------------------------------------------------------------
-|
-| Here is where you can register web routes for your application. These
-| routes are loaded by the RouteServiceProvider within a group which
-| contains the "web" middleware group. Now create something great!
-|
-*/
-
 Route::get('/', function () {
     return view('welcome');
 });
-
 Route::get('index',[
     'as'=>'trang-chu',
     'uses'=>'PageController@getIndex'
 ]);
-
 Route::get('loai-san-pham/{type}',[
     'as' =>'loaisanpham',
     'uses'=>'PageController@getLoaiSp'
@@ -41,12 +28,10 @@ Route::get('gioi-thieu',[
     'as' =>'gioithieu',
     'uses'=>'PageController@getGioiThieu'
 ]);
-
 Route::get('add-to-cart/{id}',[
     'as' =>'themgiohang',
     'uses'=>'PageController@getAddtoCart'
 ]);
-
 Route::get('del-cart/{id}',[
     'as' =>'xoagiohang',
     'uses'=>'PageController@getDelItemCart'
@@ -81,7 +66,6 @@ Route ::get('dang-xuat',[
     'as'=>'logout',
     'uses'=>'PageController@postLogout'
 ]);
-
 Route ::get('search',[
     'as'=>'search',
     'uses'=>'PageController@getSearch'
